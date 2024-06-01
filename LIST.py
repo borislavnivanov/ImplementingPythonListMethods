b31fc4f08143bb57d083091b6c0a82c5b9671b95
Borislav Ivanov <borislavnivanov@gmail.com>
2024-06-02 00:01:33 +0300

    Documentation added

--- LIST.py
+++ LIST.py
@@ -98,7 +98,11 @@ def main():
         print(lst)
 
 
-def display_menu():
+def display_menu() -> None:
+    """
+    General Menu
+    :return: NONE
+    """
     print("\nChoose a list operation:")
     print("1. Append")
     print("2. Extend")
@@ -115,16 +119,35 @@ def display_menu():
 
 
 def append(base_list: list, new_element: Any) -> list:
+    """
+    Places and value in a position at the end of the list
+    :param base_list:
+    :param new_element:
+    :return:
+    """
     base_list.append(new_element)
     return base_list
 
 
 def extend(base_list: list, new_element) -> list | TypeError:
+    """
+    Places multiple items to the end of the list
+    :param base_list:
+    :param new_element:
+    :return:
+    """
     base_list.extend(new_element)
     return base_list
 
 
 def insert(base_list: list, new_element: Any, place: int) -> list | IndexError:
+    """
+    Inputs an element at an index
+    :param base_list:
+    :param new_element:
+    :param place:
+    :return:
+    """
     if place > len(base_list):
         raise IndexError
     else:
@@ -133,51 +156,107 @@ def insert(base_list: list, new_element: Any, place: int) -> list | IndexError:
 
 
 def remove(base_list: list, value: Any) -> list | ValueError:
+    """
+    Removes a value
+    :param base_list:
+    :param value:
+    :return:
+    """
     base_list.remove(value)
     return base_list
 
 
 def pop(base_list: list, place: int) -> list | IndexError:
+    """
+    Removes specific index position
+    :param base_list:
+    :param place:
+    :return:
+    """
     base_list.pop(place)
     return base_list
 
 
 def clear(base_list: list):
+    """
+    Destroys the current list
+    :param base_list:
+    :return:
+    """
     base_list.clear()
 
 
 @dispatch(list, Any)
 def index(base_list: list, value: Any) -> int | ValueError:
+    """
+    Returns first index of a value in a list
+    :param base_list:
+    :param value:
+    :return: int
+    """
     x = base_list.index(value)
     return x
 
 
 @dispatch(list, Any, int)
 def index(base_list: list, value: Any, start: int, end: int) -> int | ValueError:
+    """
+    Returns first index of a value in a range within the lisr
+    :param base_list:
+    :param value:
+    :param start:
+    :param end:
+    :return: int
+    """
     x = base_list.index(value, start, end)
     return x
 
 
 def count(base_list: list, value: int) -> int:
+    """
+    Returns count of values
+    :param base_list:
+    :param value:
+    :return: int
+    """
     x = base_list.count(value)
     return x
 
 
 def sort(base_list: list) -> list:
+    """
+    Returns a sorted version of the list
+    :param base_list:
+    :return:
+    """
     base_list.sort()
     return base_list
 
 
 def reverse(base_list: list) -> list:
+    """
+    Returns a reversed version of the list
+    :param base_list: list
+    :return: list
+    """
     base_list.reverse()
     return base_list
 
 
 def copy(base_list: list) -> list:
+    """
+    Returns shallow copy of the list
+    :param base_list:
+    :return:
+    """
     return base_list.copy()
 
 
 def get_index() -> int | ValueError:
+    """
+    Geta an index from user
+    :return:
+    """
     place = int(input('Enter index: '))
     return place
 
